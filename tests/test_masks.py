e5222ec313b21a96a66470a09ee273f354e5e94b
Your Name <akusilda@proton.me>
2026-01-14 19:58:42 +0300

    fictions have been written for test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -29,17 +29,18 @@ def test_mask_card(card_number, expected_card):
     assert get_mask_card_number(card_number) == expected_card
 
     # ______________БЛОК ТЕСТА С ФИКСТУРОЙ______________
-    def test_mask_card_executed(operation_data):
-        # Pytest сам подставит сюда список из conftest.py
-        result = filter_by_state(operation_data, 'EXECUTED')
-        assert len(result) == 2
-        assert result[0]['id'] == 1
-        assert result[1]['id'] == 3
+def test_mask_card_executed(standard_card_number):
+    result = get_mask_card_number(standard_card_number)
+    assert result == "8403 86** **** 5678"
 
-    def test_filter_empty(empty_list):
-        # Pytest подставит пустой список []
-        assert filter_by_state(empty_list, 'EXECUTED') == []
+def test_incorrect_card_executed(incorrect_input):
+    assert get_mask_card_number(incorrect_input) == "Ошибка: номер карты должен состоять из 16 цифр"
 
+def test_empty_card_executed(empty_input):
+    assert get_mask_card_number(empty_input) == "Ошибка: номер карты должен состоять из 16 цифр"
+
+
+    # ______________БЛОК ТЕСТА С ДАННЫМИ ВВЕДЕННЫМИ ВРУЧНУЮ______________
     """Тест для функции маскировки номера банковского счета"""
 @pytest.mark.parametrize("account_number, expected_account", [
     # 1. Проверка правильности маскирования
@@ -65,3 +66,16 @@ def test_mask_card(card_number, expected_card):
 ])
 def test_mask_account(account_number, expected_account):
     assert get_mask_account(account_number) == expected_account
+
+
+    # ______________БЛОК ТЕСТА С ФИКСТУРОЙ______________
+def test_mask_account_executed(standard_account_number):
+    result = get_mask_account(standard_account_number)
+    assert result == "**5640"
+
+
+def test_incorrect_account_executed(incorrect_input):
+    assert get_mask_account(incorrect_input) == "Ошибка: номер счета должен состоять из 20 цифр"
+
+def test_empty_account_executed(empty_input):
+    assert get_mask_account(empty_input) == "Ошибка: номер счета должен состоять из 20 цифр"
